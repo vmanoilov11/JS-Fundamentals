d2ae1f71a7fbee31d34ec03fcb5ea22c0680e3aa
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-13 18:47:00 +0200

    Add files via upload

---
+++ exercise (20).js
@@ -0,0 +1,9 @@
+function solve(input) {
+  let counter = 0
+    for (let i = 0; i < input.length; i++) {
+    counter++
+  }
+  console.log(counter);
+  console.log(input);
+}
+solve("Hello, JavaScript!");
