d65346f610499805befcd393a98b654c532309ac
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-13 18:46:52 +0200

    Add files via upload

---
+++ exercise (18).js
@@ -0,0 +1,9 @@
+function solve(name, population, treasury) {
+  let city = {
+    name: name,
+    population: population,
+    treasury: treasury,
+  };
+  return city;
+}
+solve("Tortuga", 7000, 15000);
