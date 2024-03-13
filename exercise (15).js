85d2657833a8c7de06fdfbc29eb6ce233b627c04
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-13 18:45:53 +0200

    Add files via upload

---
+++ exercise (15).js
@@ -0,0 +1,11 @@
+function solve(arr) {
+  arr.sort((a, b) => b - a);
+  let result = [];
+
+  while (arr.length !== 0) {
+    result.push(arr.pop());
+    result.push(arr.shift());
+  }
+  console.log(result);
+}
+solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
