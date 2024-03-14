d5c728cc65459c43deb4f51c0cca4ec30ccd1411
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-14 17:01:48 +0200

    Add files via upload

---
+++ exercise (21).js
@@ -0,0 +1,7 @@
+function solve(input, input2, input3) {
+    let sum = input.length + input2.length + input3.length;
+    let average = Math.round(sum/3)
+    console.log(sum)
+    console.log(average)
+}
+solve("chocolate", "ice cream", "cake");
