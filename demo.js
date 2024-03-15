c6bce52b82905cad2fa318538ab882acb02e7cdf
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-15 12:29:18 +0200

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,16 @@
+function hair(input) {
+  let target = Number(input[0]);
+  for (let i = 0; i < input.length; i++) {
+    console.log(input[i]);
+  }
+}
+hair([
+  "300",
+  "haircut",
+  "ladies",
+  "haircut",
+  "kids",
+  "color",
+  "touch up",
+  "closed",
+]);
