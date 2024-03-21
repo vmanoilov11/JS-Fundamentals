573011ee48d2983a1cc9bdd7f97e768e2f0ce87b
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-21 23:46:15 +0200

    Add files via upload

---
+++ signCheck.js
@@ -0,0 +1,16 @@
+function solve(num1 , num2 , num3){
+
+    let result = (num1 * num2 * num3)
+
+    let result = String()
+
+    for(let i = 0; i < result.length; i++){
+
+    if(result[i] === "-"){
+        console.log("Negative")
+    }else{
+        console.log("Positive")
+    }}
+    
+}
+solve( 5, 12,-15)
