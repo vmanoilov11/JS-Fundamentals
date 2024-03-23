47d9854d130fb90379499eaba05e3aeb7fdca9af
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-23 19:59:46 +0200

    Add files via upload

---
+++ rotate.js
@@ -0,0 +1,7 @@
+function solve(input, rotations) {
+  for (let i = 0; i < rotations; i++) {
+    input.unshift(input.pop());
+  }
+  console.log(input.join(" "));
+}
+solve(["1", "2", "3", "4"], 2);
