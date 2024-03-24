bd017a038ead97ed890d41c1407c9e549c3cefb9
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-24 23:06:39 +0200

    Add files via upload

---
+++ largestNumber.js
@@ -0,0 +1,8 @@
+function solve(input, input2, input3){
+    let result;
+    
+    let biggest = Math.max(input, input2, input3);
+    console.log(`The largest number is ${biggest}.`);
+
+}
+solve(5, -3, 16)
