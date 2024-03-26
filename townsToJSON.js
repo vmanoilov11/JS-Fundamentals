c00a318f8d6568aa75e291d2f33153412340cbce
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-26 23:10:29 +0200

    Add files via upload

---
+++ townsToJSON.js
@@ -0,0 +1,13 @@
+function solve(input){
+
+    let result = []
+    for(let line of input){
+        let town = line.split(" | |")
+        console.log(town)
+    }
+
+}
+solve(['| Town | Latitude | Longitude |',
+'| Sofia | 42.696552 | 23.32601 |',
+'| Beijing | 39.913818 | 116.363625 |']
+)
