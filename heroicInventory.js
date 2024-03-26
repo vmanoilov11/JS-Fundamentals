0ad2bae3fc6704ead8f72d5a2490e597358b2e52
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-26 23:10:42 +0200

    Add files via upload

---
+++ heroicInventory.js
@@ -0,0 +1,21 @@
+function solve(input) {
+  let result = [];
+
+  for (let element of input) {
+    let [name, level, items] = element.split(" / ");
+    level = +level;
+    if (items === undefined) {
+      items = [];
+    } else {
+      items = items.split(", ");
+    }
+
+    result.push({ name, level, items });
+  }
+  console.log(JSON.stringify(result));
+}
+solve([
+  "Isacc / 25 / Apple, GravityGun",
+  "Derek / 12 / BarrelVest, DestructionSword",
+  "Hes / 1",
+]);
