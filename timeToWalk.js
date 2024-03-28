3926c2b92681b375f156d962637ff8f2bc853994
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-28 18:36:29 +0200

    Add files via upload

---
+++ timeToWalk.js
@@ -0,0 +1,22 @@
+function solve(steps, footprint, kmH){
+
+    let roadToSchool = steps * footprint
+    let mSec = kmH * 3.6
+    let time = roadToSchool / mSec
+    let rest = Math.floor(roadToSchool / 500)
+
+    let timeMin = Math.floor(time / 60)
+    let timeSec = Math.round(time - timeMin * 60)
+    let timeHour = Math.floor(time / 3600)
+
+    console.log(
+        (timeHour < 10 ? "0" : "") +
+     timeHour +
+     ":" + 
+    (timeMin + rest < 10 ? "0" : "") +
+    (timeMin + rest) + 
+    ":" +
+    (timeSec < 10 ? "0" : "") + timeSec)
+
+}
+solve(2564, 0.70, 5.5)
