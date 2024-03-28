47be6e22a3d7cdac18d16de055f3d732614bf4d2
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-03-28 18:34:30 +0200

    Add files via upload

---
+++ cityTax.js
@@ -0,0 +1,11 @@
+function solve(input) {
+  let result = {};
+  for (let i = 0; i < input.length; i++) {
+    if (i % 2 === 0) {
+      result[input[i]] = +input[i + 1];
+    }
+  }
+  console.log(result);
+}
+solve(["Yoghurt", "48", "Rise", "138", "Apple", "52"]);
+solve(["Potato", "93", "Skyr", "63", "Cucumber", "18", "Milk", "42"]);
