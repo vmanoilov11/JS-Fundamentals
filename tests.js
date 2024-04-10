d0fd6d2f581a681ecd245f2d2996e03a79106631
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-04-10 23:29:52 +0300

    Add files via upload

--- tests.js
+++ tests.js
@@ -1,81 +1,77 @@
-describe("Book Selection", () => {
-  describe("Test for isGenreSuitable", () => {
-    it("Check for invalid parameters", () => {
-      expect(() =>
-        bookSelection
-          .isGenreSuitable("Crime", "number")
-          .to.throw("Invalid input!")
-      );
-      expect(() =>
-        bookSelection.isGenreSuitable(11, 12).to.throw("Invalid input!")
-      );
-      expect(() =>
-        bookSelection.isGenreSuitable(3, "Horror").to.throw("Invalid input!")
+describe("companyAdministration", () => {
+  describe("hiringEmployee", () => {
+    it("position is not Programmer", () => {
+      expect(() => companyAdministration.hiringEmployee("A", "A", 1)).to.throw(
+        "We are not looking for workers for this position."
       );
     });
-
-    it("Check valid parameters", () => {
-      expect(bookSelection.isGenreSuitable("Horror", 12)).to.equal(
-        `Books with Horror genre are not suitable for kids at 12 age`
-      );
-      expect(bookSelection.isGenreSuitable("Horror", 20)).to.equal(
-        `Those books are suitable`
-      );
+    it("check if experience is below 3", () => {
+      expect(
+        companyAdministration.hiringEmployee("A", "Programmer", 1)
+      ).to.equal("A is not approved for this position.");
+    });
+    it("check if experience is greater than 3", () => {
+      expect(
+        companyAdministration.hiringEmployee("A", "Programmer", 4)
+      ).to.equal("A was successfully hired for the position Programmer.");
+    });
+    it("check if experience is 3", () => {
+      expect(
+        companyAdministration.hiringEmployee("A", "Programmer", 3)
+      ).to.equal("A was successfully hired for the position Programmer.");
     });
   });
-
-  describe("Test for isItAffordable", () => {
-    it("Check valid parameters", () => {
-      expect(bookSelection.isItAffordable(20, 21)).to.equal(
-        `Book bought. You have 1$ left`
+  describe("calculateSalary", () => {
+    it("check if hours is not a number", () => {
+      expect(() => companyAdministration.calculateSalary("a")).to.throw(
+        "Invalid hours"
       );
-      expect(bookSelection.isItAffordable(15, 10)).to.equal(
-        `You don't have enough money`
+    });
+    it("check if hours is a negative number", () => {
+      expect(() => companyAdministration.calculateSalary(-1)).to.throw(
+        "Invalid hours"
       );
+    });
+    it("check if hours is valid and below 160", () => {
+      expect(companyAdministration.calculateSalary(100)).to.equal(15 * 100);
+    });
+    it("check if hours is valid and greater than 160", () => {
+      expect(companyAdministration.calculateSalary(161)).to.equal(
+        15 * 161 + 1000
+      );
+    });
+    it("check if hours is valid equal 160", () => {
+      expect(companyAdministration.calculateSalary(160)).to.equal(15 * 160);
     });
   });
-
-  describe("Test for suitableTitles", () => {
-    it("Check for invalid parameters", () => {
-      expect(() =>
-        bookSelection
-          .suitableTitles("array", "genre")
-          .to.throw("Invalid input!")
+  describe("firedEmployee", () => {
+    it("check with invalid inputs", () => {
+      expect(() => companyAdministration.firedEmployee(1, 1)).to.throw(
+        "Invalid input"
       );
-      expect(() =>
-        bookSelection
-          .suitableTitles("genre", ["Horror", "Adventure", "Crime"])
-          .to.throw("Invalid input!")
+      expect(() => companyAdministration.firedEmployee([], "1")).to.throw(
+        "Invalid input"
       );
-      expect(() =>
-        bookSelection
-          .suitableTitles(["Horror", "Adventure", "Crime"], 2)
-          .to.throw("Invalid input!")
+      expect(() => companyAdministration.firedEmployee([], -1)).to.throw(
+        "Invalid input"
+      );
+      expect(() => companyAdministration.firedEmployee(["Peter"], 1)).to.throw(
+        "Invalid input"
+      );
+      expect(() => companyAdministration.firedEmployee(["Peter"], [])).to.throw(
+        "Invalid input"
+      );
+      expect(() => companyAdministration.firedEmployee("1", 1)).to.throw(
+        "Invalid input"
       );
     });
-
-    it("Check for valid parameters", () => {
+    it("check with valid inputs", () => {
       expect(
-        bookSelection.suitableTitles(
-          [
-            { title: "The Da Vinci Code", genre: "Crime" },
-            { title: "Presumed Innocent", genre: "Crime" },
-          ],
-          "Crime"
-        )
-      ).to.deep.equal(["The Da Vinci Code", "Presumed Innocent"]);
+        companyAdministration.firedEmployee(["Peter", "Ivan"], 1)
+      ).to.equal("Peter");
       expect(
-        bookSelection.suitableTitles(
-          [{ title: "The Guest List", genre: "Thriller" }],
-          "Thriller"
-        )
-      ).to.deep.equal(["The Guest List"]);
-      expect(
-        bookSelection.suitableTitles(
-          [{ title: "The Da Vinci Code", genre: "Crime" }],
-          "none"
-        )
-      ).to.deep.equal([]);
+        companyAdministration.firedEmployee(["Peter", "Ivan", "John"], 0)
+      ).to.equal("Ivan, John");
     });
   });
 });
