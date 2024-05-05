0be5ea00f6dd7fc2b280ee2e196542f3447d2636
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-05-05 23:53:11 +0300

    Add files via upload

--- triangleArea.js
+++ triangleArea.js
@@ -1,9 +1,12 @@
 function solve(a, b, c) {
+
   let s = (a + b + c) / 2;
 
-  let area = Math.sqrt(s * ((s - a) * (s - b) * (s - c)));
+  let area = Math.sqrt(s*((s-a)*(s-b)*(s-c)));
 
-  console.log(area);
+  //(s - a)(s - b)(s - c)
+
+  console.log(area)
 }
 solve(2, 3.5, 4);
 solve(3, 5.5, 4);
