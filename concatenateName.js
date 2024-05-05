2caf1467696ad6b37c91a99d08d7593613ccecf0
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-05-05 23:53:31 +0300

    Add files via upload

---
+++ concatenateName.js
@@ -0,0 +1,16 @@
+function solve(first , second , delimeter){
+
+    console.log(`${first}${delimeter}${second}`)
+}
+solve('John',
+'Smith',
+'->'
+)
+solve('Jan',
+'White',
+'<->'
+)
+solve('Linda',
+'Terry',
+'=>'
+)
