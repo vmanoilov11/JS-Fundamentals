180e686f100997bbfd80e3564772bf260fb268ad
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-05-14 00:14:19 +0300

    Update timeToWalk.js

--- timeToWalk.js
+++ timeToWalk.js
@@ -1,13 +1,13 @@
 function solve(steps, footprint, kmH){
 
-    let roadToSchool = steps * footprint
-    let mSec = kmH * 3.6
-    let time = roadToSchool / mSec
-    let rest = Math.floor(roadToSchool / 500)
+    let roadToSchool = steps * footprint;
+    let mSec = kmH * 3.6;
+    let time = roadToSchool / mSec;
+    let rest = Math.floor(roadToSchool / 500);
 
-    let timeMin = Math.floor(time / 60)
-    let timeSec = Math.round(time - timeMin * 60)
-    let timeHour = Math.floor(time / 3600)
+    let timeMin = Math.floor(time / 60);
+    let timeSec = Math.round(time - timeMin * 60);
+    let timeHour = Math.floor(time / 3600);
 
     console.log(
         (timeHour < 10 ? "0" : "") +
@@ -16,7 +16,7 @@ function solve(steps, footprint, kmH){
     (timeMin + rest < 10 ? "0" : "") +
     (timeMin + rest) + 
     ":" +
-    (timeSec < 10 ? "0" : "") + timeSec)
+    (timeSec < 10 ? "0" : "") + timeSec);
 
 }
 solve(2564, 0.70, 5.5)
